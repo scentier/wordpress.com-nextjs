e3ed04e86368837002aad82ae04052ba50011c1d
unknown <142760953+scentier@users.noreply.github.com>
2023-09-28 08:12:12 +0700

    author comment img

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,9 @@
 import { formatDateToString, slugDateFormat } from "@/lib/functions";
+import { TPost } from "@/lib/types";
 import { API_URI, BLOG_ID } from "@/lib/variables";
 import Link from "next/link";
+import { AiOutlineClockCircle } from "react-icons/ai";
+import { FaCommentAlt } from "react-icons/fa";
 
 async function getPosts<T>() {
   const postNumber = 10;
@@ -14,49 +17,59 @@ async function getPosts<T>() {
   return data;
 }
 
-type TAuthor = {
-  name: string;
-};
-
-type TComment = {
-  comment_count: number;
-};
-
-type TPost = {
-  ID: number;
-  title: string;
-  slug: string;
-  date: string;
-  excerpt: string;
-  featured_image?: string;
-  author: TAuthor;
-  discussion: TComment;
-};
-
 export default async function Home() {
   const data = await getPosts();
   const posts: TPost[] = await data.posts;
   return (
     <>
       {posts.map((post, index) => (
-        <div key={post.ID}>
-          <p>index: {index}</p>
-          {post.featured_image && (
-            <div>{`${post.featured_image}?resize=350,350`}</div>
+        <div key={post.ID} className="mx-3 my-5">
+          {index === 0 ? (
+            <>
+              <img
+                className="w-full mb-3"
+                src={`${post.featured_image}?resize=900,450`}
+              />
+              <h2 className="pb-5 my-4 border-b-[1px] border-slate-200 text-3xl text-sky-600 hover:text-sky-500">
+                <Link href={`${slugDateFormat(post.date)}/${post.slug}`}>
+                  {post.title}
+                </Link>
+              </h2>
+            </>
+          ) : (
+            <>
+              <p>index: {index}</p>
+              {post.featured_image && (
+                <div>{`${post.featured_image}?resize=350,350`}</div>
+              )}
+              <h2 className="my-5 text-4xl font-normal text-slate-900 hover:text-slate-600">
+                <Link href={`${slugDateFormat(post.date)}/${post.slug}`}>
+                  {post.title}
+                </Link>
+              </h2>
+              <div dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+              <div className="flex flex-row justify-between my-2 text-slate-400 text-sm">
+                <p className="flex flex-row items-center">
+                  <img
+                    className="mr-3 relative rounded-full h-8 w-8 overflow-hidden"
+                    src={post.author.avatar_URL}
+                  />
+                  {post.author.name}
+                </p>
+                <p className="flex flex-row items-center">
+                  <span className="mr-1 flex items-center">
+                    <AiOutlineClockCircle size={14} className="mr-1" />
+                    {formatDateToString(post.date)}
+                  </span>
+                  {" / "}
+                  <span className="ml-1 flex items-center">
+                    <FaCommentAlt size={11} className="mr-1" />
+                    {post.discussion.comment_count}
+                  </span>
+                </p>
+              </div>
+            </>
           )}
-          <h2 className="text-4xl font-normal mx-3 my-5 text-sky-600 hover:text-sky-500">
-            <Link href={`${slugDateFormat(post.date)}/${post.slug}`}>
-              {post.title}
-            </Link>
-          </h2>
-          <div dangerouslySetInnerHTML={{ __html: post.excerpt }} />
-          <div>
-            <p>{post.author.name}</p>
-            <p>
-              <span>{formatDateToString(post.date)}</span>/
-              {post.discussion.comment_count}
-            </p>
-          </div>
         </div>
       ))}
     </>
