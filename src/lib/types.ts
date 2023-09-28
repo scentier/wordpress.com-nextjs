5970dc61b8a90be0cf76dd9e9e2f9e6d9813280a
unknown <142760953+scentier@users.noreply.github.com>
2023-09-28 08:11:44 +0700

    types

---
+++ src/lib/types.ts
@@ -0,0 +1,19 @@
+type TAuthor = {
+  name: string;
+  avatar_URL: string;
+};
+
+type TComment = {
+  comment_count: number;
+};
+
+export type TPost = {
+  ID: number;
+  title: string;
+  slug: string;
+  date: string;
+  excerpt: string;
+  featured_image?: string;
+  author: TAuthor;
+  discussion: TComment;
+};
