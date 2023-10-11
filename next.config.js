879e43f78dc0693d6557e563b8931753218e15f0
unknown <142760953+scentier@users.noreply.github.com>
2023-10-11 11:24:42 +0700

    output export

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
-const envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true;
 const hosts = ["0.gravatar.com", "1.gravatar.com", "2.gravatar.com"];
 
 const nextConfig = {
-  output: process.env.NODE_ENV !== "production" ? undefined : "export",
+  output: "export",
   images: {
     remotePatterns: hosts.map((host) => ({
       protocol: "https",
       hostname: host,
     })),
 
-    unoptimized: envImageUnoptimize,
+    unoptimized: false,
   },
 };
 
